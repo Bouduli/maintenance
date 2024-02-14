b04c6d80bcb967cb601eef5fbb54af34fb2a4439
Oliver Isaksson <127422659+Bouduli@users.noreply.github.com>
2024-02-14 10:41:37 +0100

    feat: PUT/task/:id

--- api_express/routers/tasks.js
+++ api_express/routers/tasks.js
@@ -108,6 +108,55 @@ router.delete("/:id", async(req,res)=>{
     }
 })
 
+//update
+router.put("/:id", async (req,res)=>{
+
+    try {
+        const {id} = req.params;
+        const {houseID, completed, description} = req.body;
+
+        if(!id) return res.status(400).json({
+            error: "no id provided for delete"
+        });
+        //retreive existing entry (also used to check that the ID exists)
+        const find_sql = "SELECT * FROM tasks WHERE taskID = ?";
+        const found = await db.query(find_sql, [id]);
+
+        if(!found.length) return res.status(404).json({
+            error: "item not found with specified id",
+            id
+        });
+
+        //found is always an array, but should only hold one value.
+        const old = found[0];
+        const updated = {
+            taskID: old.taskID,
+            houseID : houseID || old.houseID,
+            completed : completed || old.completed,
+            description : description || old.description
+        }
+
+        const updated_sql = "UPDATE tasks SET houseID = ? , completed = ?, description = ? WHERE taskID = ?"
+        const data = await db.query(updated_sql, [ updated.houseID, updated.completed, updated.description, updated.taskID]);
+        
+        if(!data.changedRows) return res.status(304).json({
+            error:"nothing changed",
+            id
+        });
+        
+        return res.status(200).json({
+            content: {
+                id: id
+            }
+        });
+
+    } catch (err) {
+        console.log("Err @DELETE/:id  : ", err);
+        return res.status(500).json({
+            error: "something went wrong"
+        })
+    }
+})
 
 
 
--- db_mysql/sql_queries/tasks.sql
+++ db_mysql/sql_queries/tasks.sql
@@ -2,6 +2,7 @@ DROP TABLE IF EXISTS Tasks;
 CREATE TABLE Tasks (
     taskID INT AUTO_INCREMENT PRIMARY KEY,
     houseID INT NOT NULL,
+    completed BOOLEAN DEFAULT false,
     description varchar(255) NOT NULL,
     -- due_date DATE NOT NULL,
     FOREIGN KEY (houseID) REFERENCES Houses(houseID)
