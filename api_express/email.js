8e34d830ad5ecdc1b1ddfb7258daed7112291eb8
Oliver Isaksson <127422659+Bouduli@users.noreply.github.com>
2024-02-16 12:02:15 +0100

    feat: email.sendHttpEmail() UNTESTED ----- added a function to send an email with http, it is untested, however well documented with jsdoc.

--- api_express/email.js
+++ api_express/email.js
@@ -34,7 +34,34 @@ async function send(address){
 
 
     })
+}
+/**
+ * @param {string} email Recipient address
+ * @param {Object} content Content object to be displayed in an email
+ * @param {String} content.Header Header of the email, will be displayed email subject and HTTP body.
+ * @param {String} content.Body Body of the email should be put in the email text field and in HTTP body.
+ * @param {String} [content.Footer] Optional footer that will only be visible in HTTP mail.
+ * @returns {Promise<err|info>} Promise containing either a successful email or an error, if something went wrong.
+ */
+async function sendHttpMail(email, content){
+    return new Promise(function(resolve,reject){
 
+
+        const headers = {
+            from: process.env.EMAIL_USER,
+            to: email,
+            subject: "Test email",
+            text: "This is a test email, testing functionality of the email functionality."
+        };
+    
+        transporter.sendMail(headers, (err,info)=>{
+            console.log("Err: ", err);
+            console.log("info: ", info)
+            if(err) return reject(err);
+            if(info) return resolve(info);
+        })
+
+    })
 }
 
 module.exports = {send}
