6363d2e4dbea581066483d8e974703e45616a473
Oliver Isaksson <127422659+Bouduli@users.noreply.github.com>
2024-02-19 15:45:22 +0100

    changes: latest commit did not include the latest changes, this includes both routes

--- api_express/routers/contractors.js
+++ api_express/routers/contractors.js
@@ -6,14 +6,58 @@ const db = require("../db");
 //index
 router.get("/", async(req,res)=>{
 
-    const data = db.query("SELECT * FROM contractors");
+    try {
+        const data = db.query("SELECT * FROM contractors");
+        
+        if(!data.length) return res.status(404).json({
+            error: "not found"
+        })
+        return res.status(200).json({
+            content: {
+                data : data
+            }
+        });
+        
+    } catch (err) {
+        
+        console.log("err @ GET/CONTRACTORS/ :", err);
 
-
-
+        return res.status(500).json({
+            error:"internal server error"
+        });
+    }
 })
 
 //show
 router.get("/:id", async (req,res)=>{
-    const {id} = req.params;
+    try {
+        const {id} = req.params;
+    
+        if(!id) return res.status(400).json({
+            error:"no id provided"
+        });
+    
+        const sql = "SELECT * FROM contractors WHERE contractorID = ?";
+        const data = db.query(sql, [id]);
+    
+        if(!data.length) return res.status(404).json({
+            error:"resource not found with requested id",
+            id: id
+        });
+    
+        return res.status(200).json({
+            content:{
+                data: data
+            }
+        });
+    } catch (err) {
+        console.log("err @ GET/contractors/:id  : ", err);
+
+        return res.status(500).json({
+            error: "internal server error"
+        });
+
+    }
+
 
 })
