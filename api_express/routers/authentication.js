9ee39b98b2f46aaae929d791863a2176bd5bcdd3
Oliver Isaksson <127422659+Bouduli@users.noreply.github.com>
2024-02-22 11:23:02 +0100

    progress: registration --- registration route is on the way, email, password and name is retreived from body. email is validated with regex, and a sql query is fired with regards to the provided email.

--- api_express/routers/authentication.js
+++ api_express/routers/authentication.js
@@ -1,3 +1,57 @@
+//module.exports = router;
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
 
-router.post("")
+const db = require("../db");
+
+router.post("/register", async(req,res)=>{
+
+    try {
+        
+        const {email, password, name} = req.body;
+
+        //sends a 400 response object, indicating what parameters ws missing...
+        if(!email || !password || !name) {
+
+            let required = [];
+
+            if(!email) required.push("email");
+            if(!password) required.push("password");
+            if(!name) required.push("name");
+
+            return res.status(400).json({
+                error:"not all parameters were provided",
+                required
+            });
+        }
+
+        //regex to validate if something is an email. Source: https://emailregex.com/index.html
+        const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        if(!emailRegex.exec(email)) return res.status(400).json({
+            error:"bad email was provided",
+            email: email
+        });
+
+        const select_sql = "SELECT * FROM USERS WHERE email = ?";
+        const select_result = await db.query(select_sql, [email]);
+
+        return res.status(200).json(select_result);
+
+
+    } catch (err) {
+        
+        console.log("Err @ POST auth/register")
+        return res.status(500).json({
+            error:"internal server error"
+        })
+    }
+
+});
+
+
+router.post("login", async (req,res)=>{
+
+
+});
+
+module.exports = router;
