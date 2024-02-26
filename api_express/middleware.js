8f729815b5d1ed2bad34b6d8dab13e90ef82e09c
Oliver Isaksson <127422659+Bouduli@users.noreply.github.com>
2024-02-26 14:42:18 +0100

    docs: JsDoced the middlewares...

--- api_express/middleware.js
+++ api_express/middleware.js
@@ -3,6 +3,10 @@
 // access control middlewares
 const jwt = require("jsonwebtoken");
 
+/**
+ * The function returns a middleware that controls that a user is loggedIn.
+ * @returns the middleware
+ */
 function loggedIn(){
     return async function(req,res,next){
         
@@ -11,21 +15,28 @@ function loggedIn(){
         if(!cookie) return res.status(401).json({
             error: "authentication required"
         });
-        console.log(cookie);
-        console.log(process.env.STATEFUL_JWT);
-        const isValid = await verify(cookie, process.env.STATEFUL_JWT);
-        console.log(isValid);
+
+        const isValid = await verify(cookie, process.env.JWT_SECRET);
+
         if(!isValid) return res.status(401).json({
-            error:"Invalid token"
+            error:"Invalid token, please re-authenticate."
         });
+
         return next();
     }
 }
 
-//function is used
+/**
+ * Verifies a JWT token with the provided secret. 
+ * @param {*} token JWT
+ * @param {*} secret Provided secret, which changes depending on what token is used to sign the token. 
+ * @returns True, if the token can be verified; otherwise false. 
+ * 
+ */
 async function verify(token, secret){
    try {
-        const isValid = await jwt.verify(token,secret);
+        //an exception is thrown when a JWT isn't valid, hence the catch only returns false. 
+        await jwt.verify(token,secret);
         
         return true;
     } catch (err) {
