37abf22939cef71ea3f2d2d61bef421c802be261
Oliver Isaksson <127422659+Bouduli@users.noreply.github.com>
2024-02-27 20:45:37 +0100

    feat: implemented task_contractors table --- the table is used to identify which tasks a contractor have been appointed; and to accomodate that, contractors.email column has become a index, and might become primary key aswell.

--- db_mysql/sql_queries/contractors.sql
+++ db_mysql/sql_queries/contractors.sql
@@ -6,6 +6,7 @@ CREATE TABLE Contractors (
     email VARCHAR(255) NOT NULL,
     phone VARCHAR(20) NOT NULL
 );
+CREATE INDEX email_idx ON Contractors (email);
 -- INSERT INTO Contractors (name, occupation, email, phone) VALUES
 -- ('Linus Korvalds', 'Linux', 'linus@torvalds.fi', "070-521 69 69"),
 -- ('Adam Dahlgren', 'Plumber', 'adaahl0431@edu.halmstad.se', "071-241 69 69")
--- db_mysql/sql_queries/task_contractors.sql
+++ db_mysql/sql_queries/task_contractors.sql
@@ -1,8 +1,8 @@
 DROP TABLE IF EXISTS Task_Contractors;
 CREATE TABLE Task_Contractors (
     taskID INT,
-    contractorID INT,
-    PRIMARY KEY (taskID, contractorID),
+    email VARCHAR(255) NOT NULL,
+    PRIMARY KEY (taskID, email),
     FOREIGN KEY (taskID) REFERENCES Tasks(taskID),
-    FOREIGN KEY (contractorID) REFERENCES Contractors(contractorID)
+    FOREIGN KEY (email) REFERENCES Contractors(email)
 );
