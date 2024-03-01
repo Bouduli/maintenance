12a08f127c0afa0e5875f70d2cfc917d088a3327
Oliver Isaksson <127422659+Bouduli@users.noreply.github.com>
2024-03-01 17:26:06 +0100

    added: table for suggested-tasks --- table is used to store suggested_tasks which have yet to be approved by the user/owner of a house.

---
+++ db_mysql/sql_queries/Suggested_tasks.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS Suggested_tasks;
+CREATE TABLE Suggested_tasks (
+    suggestionID INT AUTO_INCREMENT PRIMARY KEY,
+    houseID INT NOT NULL,
+    contractorID INT NOT NULL,
+    description varchar(255) NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    last_edited TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    -- due_date DATE NOT NULL,
+    FOREIGN KEY (houseID) REFERENCES Houses(houseID),
+    FOREIGN KEY (contractorID) REFERENCES contractors(contractorID)
+    
+);
