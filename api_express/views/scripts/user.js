f64f4f376b575610fed79e3d85dae70d61dfd326
Oliver Isaksson <127422659+Bouduli@users.noreply.github.com>
2024-03-07 18:54:10 +0100

    feat: house creation from form works. --- because a house is identified by data which isn't returned during the create (created_at and userID) then that data won't reach the client. events are therefore fired upon 'create' that will fetch the data again. for now, this is quick, but maybe the refresh-query only should fetch the newly inserted data.

--- api_express/views/scripts/user.js
+++ api_express/views/scripts/user.js
@@ -1,6 +1,11 @@
 
 console.log("User scripts loaded");
 
+//Events used to re-trigger fetching new data. 
+const insertHouseEvent = new Event("insertHouse");
+const insertTaskEvent = new Event("insertTask");
+const insertContractorEvent = new Event("insertContractor");
+
 async function createHouse(target){
 
     const body = {
@@ -14,18 +19,59 @@ async function createHouse(target){
 
     const res = await fetch("/house", {
         method:"POST",
+        body: JSON.stringify(body),
         headers:{
-            "Content-Type":"application/json"
-        },
-        body: JSON.stringify(body)
+            "Content-Type" : "application/json"
+    
+        }
     });
 
     const json = await res.json();
     if(res.ok) {
-        console.log("Inserted id: ",json.content)
+        console.log("Inserted id: ",json.content);
+        window.dispatchEvent(insertHouseEvent);
     }
 
     else console.error(json);
     
 
+}
+
+
+//returns data for the x-data tag of "root" element
+function view(){
+    return {
+        tab: "#houses",
+        houses:[],
+        tasks:[],
+        contractors:[],
+        
+        //fetch functions
+        async fetchHouses(){
+            try {
+                this.houses = (await (await fetch("/house")).json()).content 
+            } catch (err) {
+                console.error("NO HOUSES");
+                this.houses=[];
+            }
+        },
+        async fetchTasks(){
+
+            try {
+                this.tasks = (await (await fetch("/task")).json()).content
+            } catch (err) {
+                console.error("no tasks");
+                this.tasks = [];
+            }
+        },
+        async fetchContractors(){
+            try {
+                this.contractors = (await (await fetch("/contractor")).json()).content
+                
+            } catch (err) {
+                
+            }
+        }
+
+    }
 }
