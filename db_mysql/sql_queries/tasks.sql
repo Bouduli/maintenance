9ba8a56e22599c5ae344b501b74f348ec8ba5adc
Oliver Isaksson <127422659+Bouduli@users.noreply.github.com>
2024-03-09 16:03:05 +0100

    feat: delete trigger on tasks. --- if deletion of a task was attempted, task_contractors foreign key constraint would error, since the table depends on a valid reference to taskid.
    this trigger makes sure to drop entries in the task_contractors field
    before deleting tasks.

--- db_mysql/sql_queries/tasks.sql
+++ db_mysql/sql_queries/tasks.sql
@@ -13,6 +13,17 @@ CREATE TABLE Tasks (
     
 );
 
+DROP TRIGGER IF EXISTS before_delete_Task;
+DELIMITER //
+CREATE TRIGGER before_delete_Task
+BEFORE DELETE ON Tasks
+FOR EACH ROW
+BEGIN
+    DELETE FROM Task_contractors WHERE taskID = OLD.taskID;
+END;
+//
+DELIMITER ;
+
 -- INSERT INTO TASKS (houseID, description) VALUES 
 --     (1, 'Re-Paint my facade quickly'),
 --     (1, 'Fix the pavement that has a large pot-hole')
