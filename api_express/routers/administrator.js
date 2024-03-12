cd0fbf4b7a69409ab5954d1f833b27988e8c7def
Oliver Isaksson <127422659+Bouduli@users.noreply.github.com>
2024-03-12 15:01:31 +0100

    changes: changes to many routes. --- **GET /administrator/user/:id** was rewritten to deploy "details" of a user, including all their houses, invited contractors, and each task of houses.
    **POST /administrator/user/** has added a boolean 'admin' for a request,
    therefore a logged in administrator, could create another administrator.
    
    **DELETE /administrator/user/:id** has added checks so that a user
    cannot delete themselves.

--- api_express/routers/administrator.js
+++ api_express/routers/administrator.js
@@ -8,8 +8,8 @@ const email_client = require("../email");
 //index users
 router.get("/user", async(req,res)=>{
     try {
-        
-        const sql = "SELECT userID, email, name, phone, last_login, active_account, admin FROM Users";
+        //gets a overview of a user, intended to be displayed in the list.
+        const sql = "SELECT userID, email, name, admin FROM Users where active_account=1";
         const data = await db.query(sql);
 
         if(!data.length) return res.status(404).json({
@@ -28,25 +28,54 @@ router.get("/user", async(req,res)=>{
     }
 });
 
-//show users
+//show users (details of a user, showing all houses, tasks, and contractors they've created.)
 router.get("/user/:id", async(req,res)=>{
     try {
+        //id of user is retreived from params
         const {id} = req.params;
-
         if(!id) return res.status(400).json({
             error:"id not provided"
         });
-
-        const sql = "SELECT userID, email, name, phone, last_login, active_account, admin  FROM Users WHERE userID = ?";
-        const data = await db.query(sql, [id]);
-
-        if(!data.length) return res.status(404).json({
+        
+        //retreive info of a user, this will be used as a base for the response object
+        const user_sql = "SELECT userID, email, name, phone, last_login, active_account, admin  FROM Users WHERE userID = ?";
+        const user_data = await db.query(user_sql, [id]);
+        if(!user_data.length) return res.status(404).json({
             error:" no user found with the provided userID",
             id: id
         });
 
+        //there should only be one user because of database definition, therefore we select first index.
+        const user = user_data[0]
+
+        //Retreive all houses the user has created.
+        const house_sql = "SELECT * FROM houses WHERE userID = ? and active=?";
+        const house_data = await db.query(house_sql, [id, 1]);
+        //if no houses, we assign an empty array
+        if(!house_data.length) user.houses = [];
+        else user.houses = [...house_data];
+
+
+        //retreive all tasks the user has created.
+        const task_sql = "SELECT * FROM tasks WHERE userID = ? ";
+        const task_data = await db.query(task_sql, [id]);
+        //if no tasks at all, all houses are assigned an empty array 
+        if(!task_data.length) user.houses.forEach(h => h.tasks=[]);
+        //otherwise all houses get their tasks assigned
+        else {
+            user.houses.forEach(h=>h.tasks = [...task_data.filter(t=>t.houseID == h.houseID)]);
+        }
+
+
+        //retreive all "active" contractors invited by a user
+        const contractors_sql = "SELECT * FROM contractors where invited_by=? and active_account=?";
+        const contractor_data = await db.query(contractors_sql, [id, 1]);
+        if(!contractor_data.length) user.contractors=[];
+        else user.contractors = [...contractor_data];
+
+
         return res.status(200).json({
-            content: data[0]
+            content: user
         });
         
     } catch (err) {
@@ -64,8 +93,8 @@ router.get("/user/:id", async(req,res)=>{
 router.post("/user", async(req,res)=>{
     try {
         //user data is provided in request body (admin panel uses a form for this)
-        const {email, name, phone, password} = req.body;
-
+        const {email, name, phone, password, admin} = req.body;
+        
         //if the user-data is incomplete, the server returns an error indicating what is missing
         if(!email || !name || !phone || !password) {
 
@@ -98,8 +127,8 @@ router.post("/user", async(req,res)=>{
         //makes a hash of the password
         const hash = await bcrypt.hash(password, 12);
 
-        const insert_sql = "INSERT INTO users (name, email, hash, phone) VALUES (?,?,?,?)";
-        const insert_data = await db.query(insert_sql, [name, email, hash, phone]);
+        const insert_sql = "INSERT INTO users (name, email, hash, phone, admin) VALUES (?,?,?,?,?)";
+        const insert_data = await db.query(insert_sql, [name, email, hash, phone, Boolean(admin)? 1 : 0]);
         if(!insert_data.insertId) return res.status(400).json({
             error:"register failed due to malformed request."
         });
@@ -130,18 +159,29 @@ router.post("/user", async(req,res)=>{
 //destroy user; (de-activates user and anonymises personal data)
 router.delete("/user/:id", async(req,res)=>{
     try {
+        //id of authenticated user
+        const userID = req.user.token.id;
+        if(!userID) return res.status(400).json({
+            error:"unable to retreive id, please re-authenticate"
+        });
     
+        //id of delete
         const {id} = req.params;
-
         if(!id) return res.status(400).json({
             error:"id not provided"
         });
 
+        // two reasons behind this request.
+        //  1. make sure that the user exist.
+        //  2. using retreived userID to make sure that authenticated user isn't the id that is deleted.
         const find_sql = "SELECT userID  FROM Users WHERE userID = ?";
         const find_data = await db.query(find_sql, [id]);
-
         if(!find_data.length) return res.status(404).json({
             error:"no user found with the provided userID"
+        });
+        //making sure not to delete authenticated user.
+        if(find_data[0].userID == userID) return res.status(403).json({
+            error:"You cannot delete yourself bro!", userID: userID
         });
 
         const delete_sql = "UPDATE Users SET name ='***', email = '***', phone = '***', active_account=0 WHERE userID = ?";
