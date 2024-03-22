3e0f672cab7d8942563331849a9026574e37816f
Oliver Isaksson <127422659+Bouduli@users.noreply.github.com>
2024-03-22 21:44:35 +0100

    feat: task-view pretty much complete --- task-view is functionally complete, however suggestion-approval / rejection hasn't been tested yet (since there isn't meaningful data to test it on...)

--- api_express/views/scripts/user.js
+++ api_express/views/scripts/user.js
@@ -7,72 +7,73 @@ const insertTaskEvent = new Event("insertTask");
 const insertContractorEvent = new Event("insertContractor");
 const dataChangeEvent = new Event("dataChange");
 
-async function alterHouse(target, editing=false){
+async function alterHouse(target, editing = false) {
 
-   try {
-     const houseID = target.houseID.value;
-     if(editing & !houseID) return alert("houseID not provided in edit.");
- 
-     const body = {
-        address : target.address.value || null,
-        description : target.description.value || null,
-        name : target.name.value || null
-         
-     };
- 
-     console.log(body);
- 
-     const res = await fetch(`/house/${houseID}`, {
-        method: editing? "PUT":"POST",
-        body: JSON.stringify(body),
-        headers:{
-            "Content-Type" : "application/json"
-    
+    try {
+        const houseID = target.houseID.value;
+        if (editing & !houseID) return alert("houseID not provided in edit.");
+
+        const body = {
+            address: target.address.value || null,
+            description: target.description.value || null,
+            name: target.name.value || null
+
+        };
+
+        console.log(body);
+
+        const res = await fetch(`/house/${houseID}`, {
+            method: editing ? "PUT" : "POST",
+            body: JSON.stringify(body),
+            headers: {
+                "Content-Type": "application/json"
+
+            }
+        });
+        console.log(res);
+        const json = await res.json();
+        if (res.ok) {
+            console.log("Inserted house with id: ", json.content);
+
+            //This tells Alpine.JS to re-fetch the data.
+            window.dispatchEvent(dataChangeEvent);
+
+            //clears form
+            target.reset();
+            return json.content.id;
         }
-     });
-    console.log(res);
-    const json = await res.json();
-    if(res.ok) {
-        console.log("Inserted house with id: ",json.content);
-
-        //This tells Alpine.JS to re-fetch the data.
-        window.dispatchEvent(dataChangeEvent);
-
-        //clears form
-        target.reset();
-    }
-    else console.error(json);
+        else console.error(json);
     } catch (err) {
-    
+
         console.error(err);
 
     }
-    
+
 
 }
-async function alterTask(target,editing=false){
+async function alterTask(target, editing = false) {
     try {
         const taskID = target.taskID.value;
-        if(editing & !taskID) return alert("taskID not provided in edit.");
+        if (editing & !taskID) return alert("taskID not provided in edit.");
 
         const body = {
-            description : target.description.value || null,
-            houseID : target.houseID.value || null
-            
+            description: target.description.value || null,
+            houseID: target.houseID.value || null
+
         };
 
         const res = await fetch(`/task/${taskID}`, {
-            method: editing? "PUT":"POST",
+            method: editing ? "PUT" : "POST",
             body: JSON.stringify(body),
-            headers:{
-                "Content-Type" : "application/json"
-        
+            headers: {
+                "Content-Type": "application/json"
+
             }
         });
-        
+
         const json = await res.json();
 
-        if(res.ok){
+        if (res.ok) {
             console.log("altered task with id: ", json.content);
             //This tells Alpine.JS to re-fetch the data.
             window.dispatchEvent(dataChangeEvent);
@@ -85,28 +86,28 @@ async function alterTask(target,editing=false){
 
         }
         else console.error("res not ok: ", json);
-        
+
 
     } catch (err) {
         console.error("unable to alter task: ", err);
     }
 }
-async function createTask(target){
+async function createTask(target) {
     const body = {
-        houseID : target.houseID.value || null,
-        description : target.description.value || null,
-        
+        houseID: target.houseID.value || null,
+        description: target.description.value || null,
+
     };
     const res = await fetch("/task", {
-        method:"POST",
-        body:JSON.stringify(body),
-        headers:{
-            "Content-Type" : "application/json"
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+            "Content-Type": "application/json"
         }
     });
 
     const json = await res.json();
-    if(res.ok) {
+    if (res.ok) {
         console.log("inserted task with id: ", json.content);
 
         //this tells alpine to re-fetch tasks, allowing our new task to be displayed.
@@ -119,25 +120,25 @@ async function createTask(target){
 
 }
 
-async function createContractor(target){
+async function createContractor(target) {
     const body = {
-        email: target.email.value ||null,
-        name: target.name.value ||null,
-        phone: target.phone.value ||null,
-        occupation: target.occupation.value ||null,
+        email: target.email.value || null,
+        name: target.name.value || null,
+        phone: target.phone.value || null,
+        occupation: target.occupation.value || null,
     };
 
-    const res = await fetch("/contractor",{
-        method:"POST",
-        body:JSON.stringify(body),
-        headers:{
-            "Content-Type" : "application/json"
+    const res = await fetch("/contractor", {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+            "Content-Type": "application/json"
         }
     });
 
     const json = await res.json();
 
-    if(res.ok){
+    if (res.ok) {
         console.log(json);
         window.dispatchEvent(dataChangeEvent);
         target.reset();
@@ -148,26 +149,25 @@ async function createContractor(target){
 }
 
 // async function inviteContractor(target){
-async function inviteContractor(email, taskID){
+async function inviteContractor(contractorID, taskID) {
     try {
         const body = {
-            // taskID : target.taskID.value || null,
-            // email : target.email.value || null,
-            email: email,
+            contractorID: contractorID,
             taskID: taskID
-        }
+        };
 
         console.log(body);
+
         const res = await fetch("/task/invite", {
-            method:"POST",
+            method: "POST",
             body: JSON.stringify(body),
-            headers:{
+            headers: {
                 "Content-Type": "application/json"
             }
-            
+
         });
         const json = await res.json();
-        if(res.ok){
+        if (res.ok) {
             console.log(json);
 
             //note this triggers re-fetch of all tasks, however this acheives the correct action
@@ -180,7 +180,7 @@ async function inviteContractor(email, taskID){
         console.error(err);
     }
 }
-async function destroy(type , id, target){
+async function destroy(type, id, target) {
 
     // console.log("destroyType: ", type );
     // console.log("destroyId: ", id );
@@ -189,14 +189,13 @@ async function destroy(type , id, target){
     const url = `/${type}/${id}`;
     console.log("destroy url : ", url);
 
-    
     const res = await fetch(url, {
-        method:"DELETE"
+        method: "DELETE"
     })
 
     const json = await res.json();
 
-    if(res.ok){
+    if (res.ok) {
         console.log(json);
         window.dispatchEvent(dataChangeEvent);
     }
@@ -207,14 +206,14 @@ async function destroy(type , id, target){
 
 }
 
-async function handleSuggestion(id, handle_method){
+async function handleSuggestion(id, handle_method) {
     try {
         const res = await fetch(`/task/suggestion/${id}`, {
             method: handle_method
         });
         const json = await res.json();
 
-        if(res.ok){
+        if (res.ok) {
             console.log(json);
             window.dispatchEvent(dataChangeEvent);
         }
@@ -224,111 +223,140 @@ async function handleSuggestion(id, handle_method){
 }
 
 //returns data for the x-data tag of "root" element
-function view(){
+function view() {
     return {
         tab: "#houses",
-        subtab:'houseWrapper',
-        modal:false,
+        subtab: 'houseWrapper',
+        modal: false,
 
-        house:{ tasks:[]},
-        houses:[],
-
-        tasks:[],
-        filteredTasks:[],
+        house: { tasks: [] },
         // used to accurately display task_filter when going from houseDetails to Tasks with the "show tasks" button. 
-        task_filter_id:"",
-        task:{contractors:[]},
+        selected_house_id: "",
+        houses: [],
 
-        contractors:[],
-        contractor:{},
+        tasks: [],
+        filteredTasks: [],
+        task: { contractors: [] },
 
-        suggestions:[],
-        async data(){
+        contractors: [],
+        contractor: {},
+
+        suggestions: [],
+        async data() {
             await this.fetchHouses();
             await this.fetchTasks();
             await this.fetchContractors();
             await this.fetchSuggestions();
             //make house-modal update itself.
-            if(this.house.houseID) this.house.tasks = this.tasks.filter(t=>t.houseID == this.house.houseID);
+            if (this.house.houseID) {
+                this.hosue = this.houses.find(h => h.houseID == this.house.houseID) || { tasks: [] };
+                if (this.hosue.houseID) this.house = this.getHouse(this.house.houseID)
+            }
+            if (this.task.taskID) {
+                //re-assigning task, allows for updated information to be fetched.
+                this.task = this.tasks.find(t => t.taskID == this.task.taskID) || { contractors: [] };
+                if (this.task.taskID) this.task = await this.getTask(this.task.taskID);
+            }
+
         },
-        async fetchHouses(){
+        async fetchHouses() {
             try {
-                this.houses = (await (await fetch("/house")).json()).content 
+                this.houses = (await (await fetch("/house")).json()).content
             } catch (err) {
                 console.error("NO HOUSES");
-                this.houses=[];
+                this.houses = [];
             }
         },
-        async fetchTasks(){
+        async fetchTasks() {
 
             try {
                 const initial_length = this.tasks.length;
 
                 this.tasks = (await (await fetch("/task")).json()).content;
-                
+
                 //if there is a selected house for filtering (filteredTasks have a length, not equal to the initial length of tasks.)
-                if(this.filteredTasks.length != initial_length) {
+                if (this.filteredTasks.length != initial_length) {
                     /**
                      * @type {string} houseID used to filter tasks in taskWrapper
                      */
                     const id_for_filter = this.filteredTasks[0].houseID;
-                    if(!id_for_filter) this.filteredTasks = this.tasks;
+                    if (!id_for_filter) this.filteredTasks = this.tasks;
 
-                    this.filteredTasks = this.tasks.filter(t=>t.houseID == id_for_filter);
+                    this.filteredTasks = this.tasks.filter(t => t.houseID == id_for_filter);
                 }
                 else this.filteredTasks = this.tasks;
-                
+
             } catch (err) {
                 console.error("no tasks");
                 this.tasks = [];
                 this.filteredTasks = [];
             }
         },
-        async fetchContractors(){
+        async fetchContractors() {
             try {
                 this.contractors = (await (await fetch("/contractor")).json()).content
-                
+
             } catch (err) {
                 console.log("no contractors");
-                this.contractors=[];
+                this.contractors = [];
             }
         },
-        async fetchSuggestions(){
+        async fetchSuggestions() {
             try {
-                this.suggestions = (await(await fetch("/task/suggestion")).json()).content;
+                this.suggestions = (await (await fetch("/task/suggestion")).json()).content;
+
+                //make a suggestion functionally the same as a task...
+                this.suggestions.forEach(s=>{
+                    s.taskID = s.suggestionID;
+                });
                 // console.log(this.suggestions);
             } catch (err) {
                 console.log("no suggestions");
-                this.suggestions=[];
+                this.suggestions = [];
             }
         },
+
+        filterTasksBy(group, houseID) {
+            //if no houseID - output is unfiltered according to tasks
+            
+            if(group=="tasks") {
+                if(!houseID) return this.tasks;
+                return this.tasks.filter(t=>t.houseID == houseID);
+            }
+                
+            if(group=="suggestions") {
+                if(!houseID) return this.suggestions;
+                return this.suggestions.filter(t=>t.houseID == houseID);
+            }
+            
+        },
+
         /**
          * Retreives the house from houses-array, with tasks mapped to it.
          * @param {string} house houseID
          * @returns {house} hosue
          */
-        getHouse(house){
-            const h = this.houses.find(h=>h.houseID == house);
+        getHouse(house) {
+            const h = this.houses.find(h => h.houseID == house);
             //if no house was found with the id, then "empty house" is returned
-            if(!h) return {tasks:[]};
+            if (!h) return { tasks: [] };
 
-            h.tasks = this.tasks.filter(t=>t.houseID == house);
+            h.tasks = this.tasks.filter(t => t.houseID == house);
             return h;
         },
-
         /**
          * Retreives the task from taks-array, with it's contractors fetched (hence async)
          * @param {string} task taskID 
          * @returns {Promise<contractor>} contractor
          */
-        async getTask(task){
-            const t = this.tasks.find(t=>t.taskID == task);
-            if(!t) return {contractors:[]}
+        async getTask(task) {
+            const t = this.filteredTasks.find(t => t.taskID == task);
+            if (!t) return { contractors: [] }
 
-            
-            const appointees = await (async ()=>{
+
+            const appointees = await (async () => {
                 try {
-                    return (await(await fetch(`/task/appointee/${task}`)).json()).content || [];
+                    return (await (await fetch(`/task/appointee/${task}`)).json()).content || [];
                 } catch {
                     return []
                 }
