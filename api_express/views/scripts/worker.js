e2eebf41c0e40f62dd900af7f8de3834740304e3
Oliver Isaksson <127422659+Bouduli@users.noreply.github.com>
2024-05-05 17:26:35 +0200

    feat(contractor) viewing suggestions and details
    Utilizing route from previous commit - the app can now display created
    suggestions.
    Some labels needed some correcting to display the "type" properly -
    as almost all text references "Task", instead of retreiving it
    dynamically.

--- api_express/views/scripts/worker.js
+++ api_express/views/scripts/worker.js
@@ -75,7 +75,6 @@ function view(){
 
         filterTasksBy(group, houseID) {
             //if no houseID - output is unfiltered according to tasks
-            
             if(group=="tasks") {
                 if(!houseID) return this.tasks;
                 return this.tasks.filter(t=>t.houseID == houseID);
@@ -93,6 +92,7 @@ function view(){
             try {
                 await this.fetchHouses();
                 await this.fetchTasks();
+                await this.fetchSuggestions();
                 if(this.house.houseID) this.house = await this.getHouse(this.house.houseID);
                 if(this.task.taskID) this.task = await this.getTask(this.task.taskID);
                 console.log("houses: ", this.houses);
@@ -123,9 +123,6 @@ function view(){
         },
         async fetchTasks(){
             try {
-                const init_length = this.tasks.length;
-
-
                 const res = await fetch(`/worker/task`);
                 const json = await res.json();
 
@@ -144,12 +141,13 @@ function view(){
                 console.log("unable to fetch task:", err);
             }
         },
-
         async fetchTaskDetails(t){
             try {
                 if(!t) return console.error("no id");
                 console.log(t);
-                const res = await fetch(`/worker/task/${t}`)
+                //endpoint is different depending on if it's a task or a suggestion. 
+                const endpoint = this.filterGroup =='tasks'? 'task' : 'suggestion';
+                const res = await fetch(`/worker/${endpoint}/${t}`)
                 const json = await res.json();
 
                 if(res.ok){
@@ -163,6 +161,25 @@ function view(){
 
                 console.log(err);
                 this.task = false;
+            }
+        },
+        async fetchSuggestions(){
+            try {
+                const res = await fetch("/worker/suggestion");
+                const json = await res.json();
+
+                if(res.ok){
+                    this.suggestions = json.content;
+                    //make suggestions functionally equivalent to tasks
+                    this.suggestions.forEach(t=>{
+                        t.taskID = t.suggestionID;
+                    });
+                    console.log("suggestions: ", json.content);
+                }
+                else this.suggestions = [];
+            } catch (err) {
+                console.log(err);
+                this.suggestions = [];
             }
         },
         async getTask(task) {
