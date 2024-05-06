0ae136fd123975b16afe6486d3b10975d54be62a
Oliver Isaksson <127422659+Bouduli@users.noreply.github.com>
2024-05-06 15:07:36 +0200

    feat/fix: indentity middleware
    The identity middleware, which has been lying dormant in `middleware.js`
    since it's been written, has finally been completed. The middeleware is
    used to identify a user (on index route) if the user is "logged" in
    or not.
    
    Users with different roles, have their tokens signed with different
    secrets, therefore, the regular `mw.auth()` doesn't work on "index" as
    the `mw.auth()` middleware can't really be adapted to fit this change.
    (be adapted to check different secrets)
    
    Thusly, if a token is stored in a cookie, it's decoded (no verification)
    and the user's "role" is set accordingly (`req.user` is populated).
    However the "token" is flagged as "unsafe" and checks have been placed
    to ensure that the token is properly validated when navigating further.

--- api_express/middleware.js
+++ api_express/middleware.js
@@ -2,12 +2,15 @@
 
 // access control middlewares
 const jwt = require("jsonwebtoken");
-
 /**
- * Middleware that takes tokenType as an argument, dictating which JWT-secret is used to verify the JWT.
- * @param {[string]} tokenType optional string dictates which JWT secret is used, default = "stateful".
- * @returns A middleware of type async(req,res,next)
- * @example app.get("/restricted_endpoint", auth("admin"), async (req,res)=> ... )
+ * Identification Middleware which identifies (without verifying) the user.
+ * This is only used to ENSURE that the user has a "role" when visiting `index.pug` as the user should be redirected 
+ * depending on the user's permissions. 
+ * 
+ * Since the function doesn't "verify" the JWT provided, 
+ * the token shouldn't be trusted, so therefore this route is ONLY used to display "nav-buttons" or intiate redirect
+ * (authentication is properly handeled when redirecting/navigating to the other endpoint.) 
+ * @returns 
  */
 function identity(){
     return async function (req,res,next){
@@ -15,7 +18,7 @@ function identity(){
             const cookie = req.cookies["auth-token"];
             if(cookie){
                 const unsafe_token = await jwt.decode(cookie);
-
+                req.user = {token: unsafe_token, role: unsafe_token.role, validate_required :true};
             }
             next()
         } catch (err) {
@@ -24,11 +27,18 @@ function identity(){
         }
     }
 }
+
+/**
+ * Middleware that takes tokenType as an argument, dictating which JWT-secret is used to verify the JWT.
+ * @param {[string]} tokenType optional string dictates which JWT secret is used, default = "stateful".
+ * @returns A middleware of type async(req,res,next)
+ * @example app.get("/restricted_endpoint", auth("admin"), async (req,res)=> ... )
+ */
 function auth(tokenType = "stateful"){
     return async function(req,res,next){
         
         try {
-            if(req.user) return next();
+            if(req.user && !req.user.validate_required) return next();
 
             const cookie = req.cookies["auth-token"];
 
@@ -43,6 +53,9 @@ function auth(tokenType = "stateful"){
             if(token.admin) req.user.admin=true;
             //used to control views. 
             req.user.role = token.role;
+            
+            // remove validate required field
+            delete req.user.validate_required;
 
             return next();
             
@@ -59,8 +72,8 @@ function admin(){
     return async function(req,res,next){
         try {
             const {token, admin} = req.user;
-            
-            if(!admin) return res.status(403).json({
+
+            if(!admin || token.validate_required) return res.status(403).json({
                 error:"you are not authorized for this action "
             });
 
@@ -75,4 +88,4 @@ function admin(){
     }
 }
 
-module.exports = {auth, admin};
+module.exports = {auth, admin, identity};
