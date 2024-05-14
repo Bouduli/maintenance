6d5b5fb9d097a2b8a75adcbfaa343daacf8a024d
Oliver Isaksson <127422659+Bouduli@users.noreply.github.com>
2024-05-14 09:07:27 +0200

    refactor: rebuilt sidebar without jQuery
    the sidebar functionality has been re-written without the use of jQuery,
    as it's not really necessary.

--- api_express/public/client.js
+++ api_express/public/client.js
@@ -1,19 +1,32 @@
+/**
+ * Toggles the sidebar
+ */
+document.querySelector('#expandButton').addEventListener('click', function (ev) {
 
-$('#expandButton').on('click', function () {
+    //Classes `sidebar` and `pushable` changes the location of contents. 
 
-    $('.sidebar').toggleClass('active');
-    $('.pushable').toggleClass('pushed');
+    // the class alters `transform:` style and pushes the content in, or out of space. 
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('active');
+    const pushable = document.querySelector('.pushable');
+    pushable.classList.toggle('pushed');
 
 
+    //the "pushed" part should have a click/event for "dismissing" the sidebar.
+    //this is placed in a timeout of 0 seconds, because otherwise, the eventListener on '.pushable' will be able to trigger on the same click that opened the sidebar.
+    //Result -> Sidebar opens because of click, INSTANTLY trigger close event.
     setTimeout(() => {
-        $('.pushable').on('click', function () {
-            $('.pushable').unbind('click');
-
-            $('.sidebar').toggleClass('active');
-            $(this).toggleClass('pushed');
-        });
-    }, 0)
-
+        //this is done with a named function, as we want to de-list the event after clicking. (which is impossible with anonymous functions/lambda...)
+        pushable.addEventListener('click', toggleSidebar);
+    }, 0);
 });
 
-
+/**
+ * Toggles the sidebar again, by toggling classes AND removing the event-listener. 
+ */
+function toggleSidebar() {
+    const pushable = document.querySelector('.pushable');
+    pushable.removeEventListener('click', toggleSidebar);
+    document.querySelector('.sidebar').classList.toggle('active');
+    pushable.classList.toggle('pushed')
+}
