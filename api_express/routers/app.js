7fb3be834c95e1ff6358dbc209bc03eb668081d4
Oliver Isaksson <127422659+Bouduli@users.noreply.github.com>
2024-05-20 17:51:48 +0200

    feat: permissions handeling in pug
    Now only views that the user is supposed to see, is displayed. This can
    perhaps be used to place "contractor-profile-swap" inside sidebar

--- api_express/routers/app.js
+++ api_express/routers/app.js
@@ -3,29 +3,33 @@ const router = require("express").Router();
 const mw = require("../middleware");
 
 router.get("/", mw.identity() ,async(req,res)=>{
+    const permissions = {'user':true};
+    if(req.user.token.role=='admin') permissions['admin'] = true;
 
-
-    console.log("index user: ", req.user);
+    // console.log("index user: ", req.user);
         
     if(!req.user) return res.render("login", {title:"login"})
     if(req.user.role == 'contractor')
         return res.redirect('/worker');
-    else return res.render('index', {title: 'Maintenance System', admin: req.user.role == 'admin'});
+    else return res.render('index', {title: 'Maintenance System', permissions});
 });
 router.get("/login", async(req,res)=>{
     res.render("login", {title:"Login"});
 });
 router.get("/worker", mw.auth("pwl"), async(req,res)=>{
+    const permissions = {'worker': true};
     res.render("worker", {title: "worker"})
 });
 router.get("/user", mw.auth(), async (req,res)=>{
-    res.render("user", {title:"user"});
-});
-router.get("/user2", mw.auth(), async (req,res)=>{
-    res.render("user2", {title:"user"});
+    const permissions = {'user':true};
+    if(req.user.token.role=='admin') permissions['admin'] = true;
+
+    res.render("user", {title:"user", permissions});
 });
 router.get("/admin", mw.auth(), mw.admin(), async(req,res)=>{
-    res.render("administrator", {title:"admin"});
+    const permissions = {'user':true, 'admin':true};
+
+    res.render("administrator", {title:"admin", permissions});
 });
 
 module.exports=router;
