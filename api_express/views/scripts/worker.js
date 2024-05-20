662daeb2581a16f2de3cee00ac9e4570fd1ed79b
Oliver Isaksson <127422659+Bouduli@users.noreply.github.com>
2024-05-20 19:25:19 +0200

    fix(worker-view): removed "change-profile tab"
    Removed traces of "change-profile tab" since it was implemented in the
    sidebar instead.

--- api_express/views/scripts/worker.js
+++ api_express/views/scripts/worker.js
@@ -53,47 +53,12 @@ async function markComplete(id, setStatus, target){
 
 }
 
-
-async function swapProfile(target){
-    try {
-        console.log(target);
-
-        newID = target.profile.value;
-
-        const body = {newID, isApi:true};
-        console.log(body);
-        const res = await fetch("/auth/change_profile", {
-            method:"POST",
-            body: JSON.stringify(body),
-            headers:{
-                "Content-Type": "application/json"
-            }
-            
-        });
-
-        console.log(res);
-
-        const data = await res.json();
-        if(res.ok){
-            console.log("successfully swapped ", data.content);
-            location.reload();
-        }
-        else {
-            console.error("unsuccessful swap: ", data.content);
-        }
-
-    } catch (err) {
-        console.error(err);
-    }
-}
 function view(){
 
     return {
         tab:'',
         modal:false,
         subtab:"taskWrapper",
-
-        profiles: [],
 
         houses:[],
         house:{},
@@ -126,13 +91,10 @@ function view(){
         //fetch data
         async data(){
             try {
-                //being able to switch profiles
-                await this.fetchProfiles();
-
-
                 await this.fetchHouses();
                 await this.fetchTasks();
                 await this.fetchSuggestions();
+                this.filteredTasks = this.filterTasksBy(this.filterGroup, this.filterID);
                 if(this.house.houseID) this.house = await this.getHouse(this.house.houseID);
                 if(this.task.taskID) this.task = await this.getTask(this.task.taskID);
                 // console.log("houses: ", this.houses);
@@ -143,25 +105,8 @@ function view(){
                     if(!this.houses.find(h=>h.houseID == t.houseID))
                         this.houses.push(t.houseID);
                 })
-                this.filteredTasks = this.tasks;
             } catch (err) {
                 console.log("unable to fetch tasks: ", err);
-            }
-        },
-        async fetchProfiles(){
-            try {
-                const res = await fetch("/worker/profiles");
-                const json = await res.json();
-
-                if(res.ok){
-                    this.profiles = json.content;
-
-                }
-                else {
-                    this.profiles = [];
-                }
-            } catch (err) {
-                console.log(err);
             }
         },
         async fetchHouses(){
