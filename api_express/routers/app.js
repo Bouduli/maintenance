16e17c99275e1e5ae2029a8fd21e7bafcb4dca19
Oliver Isaksson <127422659+Bouduli@users.noreply.github.com>
2024-05-20 18:15:38 +0200

    feat: contractor profile-changer in sidebar
    This commit adds the profile-changer to the sidebar. This is a more
    intuitive place than the before implemented tab "change profile".

--- api_express/routers/app.js
+++ api_express/routers/app.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 
 const mw = require("../middleware");
+const db = require("../db");
 
 router.get("/", mw.identity() ,async(req,res)=>{
     const permissions = {'user':true};
@@ -16,9 +17,24 @@ router.get("/", mw.identity() ,async(req,res)=>{
 router.get("/login", async(req,res)=>{
     res.render("login", {title:"Login"});
 });
+
+
 router.get("/worker", mw.auth("pwl"), async(req,res)=>{
+
+    // Find alternate "profiles" (mysql accounts that the user might want to access)
+    const {email, id} = req.user.token;
+
+    const sql = "SELECT * FROM contractors WHERE email =?";
+    const data = await db.query(sql, [email]);
+    if(!data.length) return res.status(404).json({
+        error:"no accounts to swap with"
+    });
+    //assign the "current" user, allowing the client to this as selected. 
+    data.find(d=>d.contractorID == id).current=true;
+
     const permissions = {'worker': true};
-    res.render("worker", {title: "worker"})
+
+    res.render("worker", {title: "worker", permissions, profiles: data});
 });
 router.get("/user", mw.auth(), async (req,res)=>{
     const permissions = {'user':true};
